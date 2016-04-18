2936e8ef0239a831a006a04560515287f5f8afe7
w0lfis <roffe@roffe.nu>
2016-04-18 23:55:43 +0200

    more stuff

--- src/run.py
+++ src/run.py
@@ -13,7 +13,7 @@ PORT = '6600'
 PASSWORD = "password"
 ###
 # Chill test sounds
-uri = 'spotify:track:30sHxvIflGaWj7nnd3oSTo'
+uri = 'spotify:user:b584371:playlist:4zlvIZSMVmeb0N6ZgLU7sk'
 
 client = MPDClient()
 
@@ -27,17 +27,18 @@ if PASSWORD:
         client.password(PASSWORD)
     except CommandError:
         exit(1)
+client.clear()
 client.consume(1)
 client.random(1)
-# client.crossfade(2)
 client.add(uri)
 client.play()
-time.sleep(3)
-client.next()
-time.sleep(3)
-client.next()
-time.sleep(3)
-client.next()
-time.sleep(3)
+time.sleep(1)
+print(client.currentsong())
+for x in range(1, 10):
+    client.next()
+    time.sleep(0.2)
+    print(client.currentsong())
+    time.sleep(5)
+
 client.stop()
 client.disconnect()
